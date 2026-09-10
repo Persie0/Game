af3f6cc92c0e1284744cad242fa43663f9682478
Persie0 <76871553+Persie0@users.noreply.github.com>
2026-09-10 13:25:13 +0900

    fix: place app export with directives

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,10 @@ import 'package:flutter/widgets.dart';
 import 'app_controller.dart';
 import 'ui/app.dart';
 
+export 'ui/app.dart' show MuseumHeistApp;
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final controller = await AppController.create();
   runApp(MuseumHeistApp(controller: controller));
 }
-
-export 'ui/app.dart' show MuseumHeistApp;
